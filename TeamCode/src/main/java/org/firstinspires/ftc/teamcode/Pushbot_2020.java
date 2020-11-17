4664cccf22052003b68101c0d212af12cdd52074
mjmagee991 <54601318+mjmagee991@users.noreply.github.com>
2020-11-17 15:17:49 -0500

    Changed around the pushbot to try to get drive working Hardware map = big dumb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pushbot_2020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pushbot_2020.java
@@ -8,11 +8,11 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 public class Pushbot_2020 {
-    /*public DcMotor leftDrive;
+    public DcMotor leftDrive;
     public DcMotor rightDrive;
     public DcMotor leftDrive2;
     public DcMotor rightDrive2;
-    public DcMotor vertExt;
+    /*public DcMotor vertExt;
     public DcMotor tapeExt;
     public Servo foundHook1;
     public Servo foundHook2;
@@ -35,40 +35,40 @@ public class Pushbot_2020 {
         // Save reference to Hardware map
         hwMap = ahwMap;
         //Define and Initialize servos
-        testServo  = hwMap.get(CRServo.class, "testServo");
+        //testServo  = hwMap.get(CRServo.class, "testServo");
         //set servo to starting position
-        testServo.setPower(0);
+        //testServo.setPower(0);
         // Define and Initialize Motors
-        /*leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        leftDrive2 = hwMap.get(DcMotor.class, "left_drive2");
-        rightDrive2 = hwMap.get(DcMotor.class, "right_drive2");
-        vertExt = hwMap.get(DcMotor.class, "vertExt");
-        tapeExt = hwMap.get(DcMotor.class, "tapeExt");
-        stoneClaw = hwMap.get(DcMotor.class, "stoneClaw");
+        leftDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+        leftDrive2 = hwMap.get(DcMotor.class, "leftBackDrive");
+        rightDrive2 = hwMap.get(DcMotor.class, "rightBackDrive");
+        //vertExt = hwMap.get(DcMotor.class, "vertExt");
+        //tapeExt = hwMap.get(DcMotor.class, "tapeExt");
+        //stoneClaw = hwMap.get(DcMotor.class, "stoneClaw");
         //Set motor direction
         leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
         leftDrive2.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
         rightDrive2.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        vertExt.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        tapeExt.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        stoneClaw.setDirection(DcMotor.Direction.FORWARD);
+        //vertExt.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        //tapeExt.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        //stoneClaw.setDirection(DcMotor.Direction.FORWARD);
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
         rightDrive2.setPower(0);
         leftDrive2.setPower(0);
-        vertExt.setPower(0);
-        tapeExt.setPower(0);
-        stoneClaw.setPower(0);
+        //vertExt.setPower(0);
+        //tapeExt.setPower(0);
+        //stoneClaw.setPower(0);
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        vertExt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        stoneClaw.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
+        //vertExt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //stoneClaw.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
