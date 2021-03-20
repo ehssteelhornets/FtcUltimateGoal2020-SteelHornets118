1a865ae5c1aeec48877595442099146b9bb12f5a
mjmagee991 <54601318+mjmagee991@users.noreply.github.com>
2021-03-20 18:20:05 -0400

    Implemented new controller setup Split controls across two controllers Not tested Look at diagram on drive for easy comprehension

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopPOV_Linear.java
@@ -32,36 +32,14 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.update();
 
-            // Select mode
-            if (gamepad1.right_stick_button) {
-                precisionMode = !precisionMode;
-                sleep(100);
-            }
 
-            double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
+            //Gamepad 1: Claw, Intake, Rev, Shoot, Drive, Precision Mode
 
-            // Precision Mode
-            if (precisionMode) {
-                telemetry.addData("Driving", "false");
-                telemetry.update();
-                robot.driveLF.setPower(v1/4);
-                robot.driveRF.setPower(v2/4);
-                robot.driveLB.setPower(v3/4);
-                robot.driveRB.setPower(v4/4);
-            } else {
-                // Normal Mode
-                telemetry.addData("Driving", "true");
-                telemetry.update();
-                robot.driveLF.setPower(v1);
-                robot.driveRF.setPower(v2);
-                robot.driveLB.setPower(v3);
-                robot.driveRB.setPower(v4);
+            // Claw Controlled by dpad up and down
+            if (gamepad1.dpad_down) {
+                robot.openClaw(true);
+            } else if (gamepad1.dpad_up) {
+                robot.openClaw(false);
             }
 
             // Intake driven by bumpers
@@ -75,56 +53,84 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
                 robot.intake.setPower(0);
             }
 
-            // Arm driven by dpad left and right
-            if (gamepad1.dpad_left) {
-                robot.armMotor.setPower(.5);
-            } else if (gamepad1.dpad_right) {
-                robot.armMotor.setPower(-.5);
-            } else {
-                robot.armMotor.setPower(0);
-            }
-
-            // Claw Controlled by dpad up and down
-            if (gamepad1.dpad_down) {
-                robot.openClaw(true);
-            } else if (gamepad1.dpad_up) {
-                robot.openClaw(false);
-            }
-
             // Rev driven by left trigger
             robot.rev(gamepad1.left_trigger > 0);
 
             // Launch driven by right trigger
             robot.launch(gamepad1.right_trigger > 0);
 
-            // Ring lift driven by a and y
-            if (gamepad1.a) {
-                robot.lift.setPower(1);
-                //robot.liftDown();
-            } else if (gamepad1.y) {
+            // Driving with
+            double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            // Precision Mode
+            if (precisionMode) {
+                telemetry.addData("Driving", "false");
+                telemetry.update();
+                robot.driveLF.setPower(v1 / 4);
+                robot.driveRF.setPower(v2 / 4);
+                robot.driveLB.setPower(v3 / 4);
+                robot.driveRB.setPower(v4 / 4);
+            } else {
+                // Normal Mode
+                telemetry.addData("Driving", "true");
+                telemetry.update();
+                robot.driveLF.setPower(v1);
+                robot.driveRF.setPower(v2);
+                robot.driveLB.setPower(v3);
+                robot.driveRB.setPower(v4);
+            }
+
+            // Precision Mode Toggle
+            if (gamepad1.b) {
+                precisionMode = false;
+            } else if (gamepad1.x) {
+                precisionMode = true;
+            }
+
+
+            // Gamepad 2: Arm, Lift, Barrel
+
+            // Arm driven by bumpers
+            if (gamepad2.left_bumper) {
+                robot.armMotor.setPower(.5);
+            } else if (gamepad2.right_bumper) {
+                robot.armMotor.setPower(-.5);
+            } else {
+                robot.armMotor.setPower(0);
+            }
+
+            // Ring lift driven by dpad up and down
+            if (gamepad2.dpad_up) {
                 robot.lift.setPower(-1);
                 //robot.liftUp();
+            } else if (gamepad2.dpad_down) {
+                robot.lift.setPower(1);
+                //robot.liftDown();
             } else {
                 robot.lift.setPower(0);
             }
 
-            // Barrel alignment driven by x and b
-            if (gamepad1.b) {
+            // Barrel alignment driven by a, b, and y
+            if (gamepad2.y) {
                 robot.barrel(2);
-            } else if (gamepad1.x) {
-                if (precisionMode) {
-                    robot.barrel(1);
-                } else {
-                    robot.barrel(0);
-                }
+            } else if (gamepad2.b) {
+                robot.barrel(1);
+            } else if (gamepad2.a) {
+                robot.barrel(0);
             }
-
-            // Get range sensor data
-            telemetry.addData("raw ultrasonic", robot.liftSensor.rawUltrasonic());
-            telemetry.addData("raw optical", robot.liftSensor.rawOptical());
-            telemetry.addData("cm optical", "%.2f cm", robot.liftSensor.cmOptical());
-            telemetry.addData("cm", "%.2f cm", robot.liftSensor.getDistance(DistanceUnit.CM));
-            telemetry.update();
         }
+
+        // Get range sensor data
+        telemetry.addData("raw ultrasonic", robot.liftSensor.rawUltrasonic());
+        telemetry.addData("raw optical", robot.liftSensor.rawOptical());
+        telemetry.addData("cm optical", "%.2f cm", robot.liftSensor.cmOptical());
+        telemetry.addData("cm", "%.2f cm", robot.liftSensor.getDistance(DistanceUnit.CM));
+        telemetry.update();
     }
 }
